c10694a2ae576eabe4dca3050839d5c1cabbdef5
msmarjorine <marjorine13@gmail.com>
2023-05-25 11:46:10 +0300

    THe first test is ready

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,7 +1,10 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PositiveTests {
@@ -13,8 +16,8 @@ public class PositiveTests {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        //sleep for 3 seconds for demo purpose
-        sleep(3000);
+        //sleep for 2 seconds for demo purpose
+        sleep(2000);
 
         //maximize browser window
         driver.manage().window().maximize();
@@ -28,13 +31,32 @@ public class PositiveTests {
         sleep(2000);
 
         //enter username
+        WebElement username = driver.findElement(By.id("username"));
+        String validUsername = "tomsmith";
+        username.sendKeys(validUsername);
         //enter password
+        WebElement password = driver.findElement(By.name("password"));
+        String validPassword = "SuperSecretPassword!";
+        password.sendKeys(validPassword);
         //click Login button
+        WebElement loginBtn = driver.findElement((By.className("radius")));
+        loginBtn.click();
 
         //verifications:
             //check url
+        String expectedUrl = "http://the-internet.herokuapp.com/secure";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "Actual page url is not the same as expected");
             //check Logout button
+        WebElement logoutBtn = driver.findElement(By.cssSelector("a.button.radius"));
+        Assert.assertTrue(logoutBtn.isDisplayed(), "The log out button is not present");
             //check notification
+        WebElement successNtf = driver.findElement(By.id("flash"));
+        Assert.assertTrue(successNtf.isDisplayed(), "The success notification is not present");
+        String expectedMessage = "You logged into a secure area!";
+        String actualMessage = successNtf.getText();
+        //Assert.assertEquals(expectedMessage, actualMessage, "The message differs from expected");
+        Assert.assertTrue(actualMessage.contains(expectedMessage), "The message differs from expected");
 
         //close browser
         driver.quit();
